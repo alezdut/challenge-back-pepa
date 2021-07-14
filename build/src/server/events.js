58bf4b337f9223803f399bd0041ca38a80a03f79
Alejandro Zdut <Fintech@T0003540416.local>
2021-07-14 12:05:11 -0300

    deleted node_modules, build and coverage from github

--- build/src/server/events.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const onServerError = () => console.log({ message: `Server error` });
-const modules = {
-    onServerError
-};
-module.exports = modules;
--- build/src/server/routes/pokeRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const express_1 = require("express");
-const pokeController_1 = __importDefault(require("../controller/pokeController"));
-const router = express_1.Router();
-router.get('/pokemon/:name', pokeController_1.default);
-module.exports = router;
